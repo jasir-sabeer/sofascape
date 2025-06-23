fdf604ea69bca5b1579f12c7307dac5fcf0d019b
jasir sabeer <jasirsabeer52@gmail.com>
2025-06-23 23:12:40 +0530

    change8

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,22 @@
+require('dotenv').config(); 
+
 module.exports = {
   apps: [
     {
       name: 'app',
-      script: './app.js',
+      script: 'app.js',
+      exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000,
-        SESSION_SECRET: 'mySecret',
-        GOOGLE_CLIENT_ID: '50816437870-ba2h228h4fqa9v9loo5cd2nuortdb6j4.apps.googleusercontent.com',
-        GOOGLE_CLIENT_SECRET: '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',
-        GOOGLE_CALLBACK_URL: 'https://sofasacpe.3utilities.com/auth/google/callback',
-        MONGODB_URI: 'mongodb+srv://SOFASCAPE:jasir%26jasi@cluster0.9eegw.mongodb.net/sofascape?retryWrites=true&w=majority&appName=Cluster0',
-        NODEMAILER_EMAIL: 'jasirsabeer52@gmail.com',
-        NODEMAILER_PASSWORD: 'brle mfwh rtyy wdjc',
-        RAZOR_PAY_ID: 'rzp_test_PK79kuFlXvgTpG',
-        RAZOR_PAY_SECRET: 'bfeBWFRX63XCWCJ1GOwCQ1xA',
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        SESSION_SECRET: process.env.SESSION_SECRET,
+        MONGODB_URI: process.env.MONGODB_URI,
+        GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+        NODEMAILER_EMAIL: process.env.NODEMAILER_EMAIL,
+        NODEMAILER_PASSWORD: process.env.NODEMAILER_PASSWORD,
+        RAZOR_PAY_ID: process.env.RAZOR_PAY_ID,
+        RAZOR_PAY_SECRET: process.env.RAZOR_PAY_SECRET
       }
     }
   ]
