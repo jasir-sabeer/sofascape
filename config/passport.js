25ae4eaf2edd962a3b7d92ab1b852c4f3755112f
jasir sabeer <jasirsabeer52@gmail.com>
2025-06-24 12:34:46 +0530

    google auth update1d

--- config/passport.js
+++ config/passport.js
@@ -10,9 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,  
-      callbackURL: isProd
-      ? 'https://sofascape.webhop.me/auth/google/callback'
-      : 'http://localhost:3000/auth/google/callback'
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
